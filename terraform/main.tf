b0d34b8d27081d8e8a7a1dca506bef33f995a932
Daniel Andrade <10710752+danielvca@users.noreply.github.com>
2020-07-31 20:36:40 +1000

    lb

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,11 @@ module "vpc" {
   source    = "./modules/vpc"
   CIDRBlock = var.CIDRBlock
 }
+
+module "loadbalancer" {
+  source        = "./modules/loadbalancer"
+  #project       = "grp3wordpress"
+  vpc_id        = module.vpc.vpc_id
+  subnets       = module.vpc.public_subnets[*].id
+  #https_enabled = true
+}
